d180c4b387a446bd183e6469bf668aa18b0daab7
tonky0110 <tonky0110@gmail.com>
2019-02-06 16:35:21 +0900

    5.2 Home Container(ComponentWillMount -> render -> ComponentDidMount순으로 호출)

--- src/Routes/Home/HomeContainer.js
+++ src/Routes/Home/HomeContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { movieApi, tvApi } from "api";
 import HomePresenter from "./HomePresenter.js";
 
 export default class HomeContainer extends React.Component {
@@ -9,6 +10,38 @@ export default class HomeContainer extends React.Component {
     error: null,
     loading: true
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: nowPlaying }
+      } = await movieApi.nowPlaying();
+      // console.log(nowPlaying);
+
+      const {
+        data: { results: upcoming }
+      } = await movieApi.upcoming();
+      // console.log(upcoming);
+
+      const {
+        data: { results: popular }
+      } = await movieApi.popular();
+      // console.log(popular);
+      this.setState({
+        nowPlaying,
+        upcoming,
+        popular
+      });
+    } catch {
+      this.setState({
+        error: "Can't find movies information."
+      });
+    } finally {
+      this.setState({
+        loading: false
+      });
+    }
+  }
   render() {
     const { nowPlaying, upcoming, popular, error, loading } = this.state;
     return (
