18ca6366d59b3bc664c3b5e7b074fa3ad726b140
tonky0110 <tonky0110@gmail.com>
2019-02-07 22:06:42 +0900

    5.4 Detail Container && return push(/);으로 if문 제어

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -7,7 +7,23 @@ export default class DetailContainer extends React.Component {
     loading: true,
     error: null
   };
+
+  componentDidMount = async () => {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const parsedId = parseInt(id);
+
+    if (isNaN(parsedId)) {
+      return push("/"); // return push를 하면, if이면 push를 if가 아니면, return ;으로 function을 종료함.
+    }
+  };
+
   render() {
+    // console.log(this.props);
     const { result, loading, error } = this.state;
     return <DetailPresenter result={result} loading={loading} error={error} />;
   }
